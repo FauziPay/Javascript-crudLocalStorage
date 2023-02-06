f4195c0deda4acdfc9cf5524d423c01fcdd60af4
Achmad Nur Fauzi <61571729+FauziPay@users.noreply.github.com>
2023-02-06 11:57:04 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,134 @@
+// validate form inputs before submit data 
+function validateForm() {
+  var name = document.getElementById("name").value;
+  var age = document.getElementById("age").value;
+  var address = document.getElementById("address").value;
+
+  if(name == ""){
+    alert("Name is required");
+    return false;
+  }
+
+  if(age == ""){
+    alert("Age is required");
+    return false;
+  }
+  else if(age < 1){
+    alert("Age must not be zero or less than zero");
+    return false;
+  }
+
+  if(address == ""){
+    alert("Address is required");
+    return false;
+  }
+  
+  return true;
+}
+
+
+// function to show data 
+function showData() {
+  var peopleList;
+  if(localStorage.getItem("peopleList") == null){
+    peopleList = [];
+  }else{
+    peopleList = JSON.parse(localStorage.getItem("peopleList"))
+  }
+
+  var html = "";
+
+  peopleList.forEach(function (element, index) {
+    html += "<tr>";
+    html += "<td>" + element.name + "</td>";
+    html += "<td>" + element.age + "</td>";
+    html += "<td>" + element.address + "</td>";
+    html += '<td><button onclick="deleteData('+index+')" class="btn btn-danger">Delete</button><button onclick="updateData('+index+')" class ="btn btn-warning m-2">Edit</button>';
+    html += "</tr>";
+  });
+
+  document.querySelector("#crudTable tbody").innerHTML = html;
+}
+
+// loads all data from local storage when document or page loaded
+document.onload = showData();
+
+// function to add data to local storage
+function AddData(){
+  if(validateForm() == true){
+    var name = document.getElementById("name").value;
+    var age = document.getElementById("age").value;
+    var address = document.getElementById("address").value;
+
+    var peopleList;
+    if(localStorage.getItem("peopleList") == null){
+      peopleList = [];
+    }else{
+      peopleList = JSON.parse(localStorage.getItem("peopleList"));
+    }
+
+    peopleList.push({
+      name: name,
+      age: age,
+      address: address,
+    });
+
+    localStorage.setItem("peopleList", JSON.stringify(peopleList));
+    showData();
+    document.getElementById("name").value = "";
+    document.getElementById("age").value = "";
+    document.getElementById("address").value = "";
+  }
+}
+
+// function to delete data from local storage 
+function deleteData(){
+  var peopleList;
+  if(localStorage.getItem("peopleList") == null){
+    peopleList = [];
+  } else {
+    peopleList = JSON.parse(localStorage.getItem("peopleList"));
+  }
+
+  peopleList.slice(index, 1);
+  localStorage.setItem("peopleList", JSON.stringify(peopleList));
+  showData();
+}
+
+// function to update/edit data in local storage 
+function updateData(index){
+  // Submit button will hide and Update button will show for updating of data in local storage
+  document.getElementById("Submit").style.display = "none";
+  document.getElementById("Update").style.display = "block";
+
+  var peopleList;
+  if(localStorage.getItem("peopleList") == null){
+    peopleList = [];
+  } else {
+    peopleList = JSON.parse(localStorage.getItem("peopleList"));
+  }
+
+  document.getElementById("name").value = peopleList[index].name;
+  document.getElementById("age").value = peopleList[index].age;
+  document.getElementById("address").value = peopleList[index].address;
+
+  document.querySelector("#Update").onclick = function () {
+    if (validateForm() == true) {
+      peopleList[index].name = document.getElementById("name").value;
+      peopleList[index].age = document.getElementById("age").value;
+      peopleList[index].address = document.getElementById("address").value;
+
+      localStorage.setItem("peopleList", JSON.stringify(peopleList));
+
+      showData();
+
+      document.getElementById("name").value = "";
+      document.getElementById("age").value = "";
+      document.getElementById("address").value = "";
+
+      // Submit button will hide and Submit button will show 
+      document.getElementById("Submit").style.display = "block";
+      document.getElementById("Update").style.display = "none";
+    }
+  }
+}
